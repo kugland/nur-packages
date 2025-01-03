106049b8dbb8940b3976ee4ae1ea3aaff3235c9a
André Kugland <kugland@gmail.com>
2025-01-03 05:50:18 -0300

    Configure cachix workflow and update neocities-deploy.
    * Updated `.github/workflows/build.yml`:
      * Set cachixName to kugland.
      * Set nurRepo to kugland.
    
    * Updated `pkgs/neocities-deploy/default.nix`:
      * Updated version to 0.1.13.
      * Changed cargoSha256 to cargoHash with new hash.

--- pkgs/neocities-deploy/default.nix
+++ pkgs/neocities-deploy/default.nix
@@ -7,14 +7,14 @@
 }:
 buildRustPackage rec {
   pname = "neocities-deploy";
-  version = "0.1.11";
+  version = "0.1.13";
   src = fetchFromGitHub {
     owner = "kugland";
     repo = "neocities-deploy";
     rev = "v${version}";
     hash = "sha256-Xy+YfXSb2ffIu4Ost5tKDXXAOzR+kkfR5kwqdlVLo0o=";
   };
-  cargoSha256 = "sha256-xDuQQjAt14V6gNqu/YG3j1VvWFKLDPBaw2yGhqDRXD8=";
+  cargoHash = "sha256-G119UJ7b9nK4XiPtKSKcRPncpWBKRw9GRnJ1PjTAA2Q=";
   meta = with lib; {
     description = "A command-line tool for deploying your Neocities site";
     homepage = "https://github.com/kugland/neocities-deploy";
