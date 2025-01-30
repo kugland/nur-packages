eea5eb618104881a6df3975b02a829030ac539d5
André Kugland <kugland@gmail.com>
2025-01-30 14:52:00 -0300

    neocities-deploy: v0.1.14

--- flake.nix
+++ flake.nix
@@ -1,8 +1,15 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
   outputs =
     { self
+    , fenix
     , nixpkgs
     ,
     }:
@@ -10,8 +17,6 @@
       systems = [
         "x86_64-linux"
         "i686-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
         "aarch64-linux"
         "armv6l-linux"
         "armv7l-linux"
@@ -25,6 +30,7 @@
       legacyPackages = forAllSystems (system:
         import ./default.nix {
           pkgs = import nixpkgs { inherit system; };
+          fenix = fenix.outputs.packages.${system};
         });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
     };
--- pkgs/neocities-deploy/default.nix
+++ pkgs/neocities-deploy/default.nix
@@ -1,41 +1,28 @@
 { pkgs
 , lib
-, stdenvNoCC
+, fetchFromGitHub
+, fenix
+,
 }:
-stdenvNoCC.mkDerivation rec {
+let
+  toolchain = fenix.minimal.toolchain;
+  buildRustPackage =
+    (pkgs.makeRustPlatform {
+      cargo = toolchain;
+      rustc = toolchain;
+    }).buildRustPackage;
+in
+buildRustPackage rec {
   pname = "neocities-deploy";
-  version = "0.1.13";
-  src = {
-    "x86_64-linux" = pkgs.fetchzip {
-      url = "https://github.com/kugland/neocities-deploy/releases/download/v${version}/neocities-deploy-Linux-x86_64-musl.tar.gz";
-      sha256 = "sha256:0xl5nj6rb145378pqiysiv0g57skg8ydf92rqw45cfv271hizfs0";
-    };
-    "i686-linux" = pkgs.fetchzip {
-      url = "https://github.com/kugland/neocities-deploy/releases/download/v${version}/neocities-deploy-Linux-i686-musl.tar.gz";
-      sha256 = "sha256:136l3rb7gpk5dpckinyvzwk2sr8r44zhamrjv1vzmnzlrvbxbkgg";
-    };
-    "aarch64-linux" = pkgs.fetchzip {
-      url = "https://github.com/kugland/neocities-deploy/releases/download/v${version}/neocities-deploy-Linux-aarch64-musl.tar.gz";
-      sha256 = "sha256:12pkm8kjg1pqa12p9b05g106djqbrbghnrf5i0kwbzw94qz68gm4";
-    };
-    "armv7l-linux" = pkgs.fetchzip {
-      url = "https://github.com/kugland/neocities-deploy/releases/download/v${version}/neocities-deploy-Linux-arm-musl.tar.gz";
-      sha256 = "sha256:0r3gpmif47i13scr4hnzmdvd6vpqsi32jchzfrvi8gsxmanwjx5j";
-    };
-    "x86_64-darwin" = pkgs.fetchzip {
-      url = "https://github.com/kugland/neocities-deploy/releases/download/v${version}/neocities-deploy-Darwin-x86_64.tar.gz";
-      sha256 = "sha256:16mndya7s172nnr32s4vgr6iiql917szn311nk89i3lyfvaxqgna";
-    };
-    "aarch64-darwin" = pkgs.fetchzip {
-      url = "https://github.com/kugland/neocities-deploy/releases/download/v${version}/neocities-deploy-Darwin-aarch64.tar.gz";
-      sha256 = "sha256:0bcs0fay1r12liqb6w1gbvd297fda85p13476177chlimgabjm8q";
-    };
-  }."${pkgs.system}";
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src/neocities-deploy $out/bin/neocities-deploy
-    chmod +x $out/bin/neocities-deploy
-  '';
+  version = "0.1.14";
+  src = fetchFromGitHub {
+    owner = "kugland";
+    repo = "neocities-deploy";
+    rev = "v${version}";
+    hash = "sha256-sbVoFHEChwxeiIBf8rSnT5zOUEqblUTUWfFQ5SjEJv0=";
+  };
+  cargoHash = "sha256-Boy48me+SzHWB+nMWFkhlRfonHs7+xAZ/Z7hBEL0lyI=";
+  doCheck = false;
   meta = with lib; {
     description = "A command-line tool for deploying your Neocities site";
     homepage = "https://github.com/kugland/neocities-deploy";
