1e4222c9cd04c6869f0eaba6c6409f9f520da3ab
André Kugland <kugland@gmail.com>
2025-01-30 14:52:00 -0300

    neocities-deploy: v0.1.14

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
-{
-  pkgs ? import <nixpkgs> { },
+{ pkgs ? import <nixpkgs> { }
+, flakeLock ? pkgs.lib.importJSON ./flake.lock
+, fenix ? import
+    (pkgs.fetchFromGitHub (with flakeLock.nodes.fenix.locked; {
+      inherit owner repo rev;
+      hash = narHash;
+    }))
+    { }
+,
 }: rec {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
@@ -10,7 +17,7 @@
   bip39 = pkgs.callPackage ./pkgs/bip39 { };
   ffsubsync = pkgs.callPackage ./pkgs/ffsubsync { inherit auditok pysubs2; };
   my-bookmarks-pl = pkgs.callPackage ./pkgs/my-bookmarks-pl { };
-  neocities-deploy = pkgs.callPackage ./pkgs/neocities-deploy { };
+  neocities-deploy = pkgs.callPackage ./pkgs/neocities-deploy { inherit fenix; };
   pysubs2 = pkgs.callPackage ./pkgs/pysubs2 { };
   subtitlecomposer = pkgs.callPackage ./pkgs/subtitlecomposer { };
 }
