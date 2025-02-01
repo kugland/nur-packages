4bc89eff53f224b0294daf3dd37b1b4f264750f4
André Kugland <kugland@gmail.com>
2025-02-01 09:43:03 -0300

    neocities-deploy: v0.1.15

--- default.nix
+++ default.nix
@@ -1,20 +1,5 @@
-let
-  inherit (builtins) fromJSON readFile;
-
-  getFlake = name:
-    with (fromJSON (readFile ./flake.lock)).nodes.${name}.locked; {
-      inherit rev;
-      outPath = fetchTarball {
-        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-        sha256 = narHash;
-      };
-    };
-in
-
-{ pkgs ? import <nixpkgs> { }
-, fenix ? import (getFlake "fenix") { inherit pkgs; }
-,
-}: rec {
+{ pkgs ? import <nixpkgs> { } }:
+rec {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
@@ -24,7 +9,7 @@ in
   bip39 = pkgs.callPackage ./pkgs/bip39 { };
   ffsubsync = pkgs.callPackage ./pkgs/ffsubsync { inherit auditok pysubs2; };
   my-bookmarks-pl = pkgs.callPackage ./pkgs/my-bookmarks-pl { };
-  neocities-deploy = pkgs.callPackage ./pkgs/neocities-deploy { inherit fenix; };
+  neocities-deploy = pkgs.callPackage ./pkgs/neocities-deploy { };
   pysubs2 = pkgs.callPackage ./pkgs/pysubs2 { };
   subtitlecomposer = pkgs.callPackage ./pkgs/subtitlecomposer { };
 }
